e3e3e6fe7c86bb19929d34b2a9a1beed04ab3f70
k-ghale <kabinghale888@gmail.com>
2026-01-12 19:41:17 -0500

    part-1 finished

--- main.js
+++ main.js
@@ -1,73 +1,109 @@
+;(function () {
 
-class Git
-{
-    function Commit(id, message) {
-        this.id = id
-        this.parent = parent;
-        this.message =  message;
-    }
+	/**
+	 * Commit class
+	 * A single commit.
+	 *
+	 * @param {number} id  		 ID of commit.
+	 * @param {Commit} parent	 Parent Commit.
+	 * @param {string} msg 		 Commit message.
+	 */
+	function Commit(id, parent, message) {
+		this.id = id;
+		this.parent = parent;
+		this.message = message;
+	}
 
-    function Git(name){
-        this.name = name; //Repo name
-        this.lastCommitId = -1;
-        this.branches = []; 
+	/**
+	 * Branch class.
+	 * A Branch.
+	 * @param {string} name 	Branch name.
+	 * @param {number} commit   Commit it points to.
+	 */
+	function Branch(name, commit) {
+		this.name = name;
+		this.commit = commit;
+	}
 
-        var master = new Branch("master", null);
-        this.branch.push(master); 
+	/**
+	 * Git class
+	 * Represents a Git repository.
+	 *
+	 * @param {string} name Repository name.
+	 */
+	function Git(name) {
+		this.name = name; // Repo name
+		this.lastCommitId = -1; // Keep track of last commit id.
+		this.branches = []; // List of all branches.
 
-        this.HEAD = master;
-    }
+		var master = new Branch('master', null); // No commit yet, so null is passed.
+		this.branches.push(master); // Store master branch.
 
-    function Branch(name, commit) {
-        this.name = name;
-        this.commit = commit;
-    }
+		this.HEAD = master; // HEAD points to current branch.
+	}
 
-    //Git init
-    Git.prototype.commit = function(message) {
-        var commit = new Commit(++this.lastCommitId, this.HEAD.commit , message)
-        
-        //Update HEAD 
-        this.HEAD.commit = commit;
+	/**
+	 * Make a commit.
+	 * @param  {string} message Commit message.
+	 * @return {Commit}         Created commit object.
+	 */
+	Git.prototype.commit = function (message) {
+		// Increment last commit id and pass into new commit.
+		var commit = new Commit(++this.lastCommitId, this.HEAD.commit, message);
 
-        return commit;
-    }
+		// Update the current branch pointer to new commit.
+		this.HEAD.commit = commit;
 
-    //log
-    Git.prototype.log= function () {
-       var commit = this.HEAD.commit, history = [];
-       
-        while (commit) {
-           history.push(commit);
+		return commit;
+	};
 
-            commit = commit.parent;
-        }
-    
-        return history;
-    }
+	/**
+	 * Checkout a branch or creates one if not present.
+	 *
+	 * @param  {string} branchName Branch to switch to or create.
+	 * @return {Branch}            Current branch after checkout.
+	 */
+	Git.prototype.checkout = function (branchName) {
+		// Loop through all branches and see if we have a branch
+		// called `branchName`.
+		for (var i = this.branches.length; i--;){
+			if (this.branches[i].name === branchName) {
+				// We found an existing branch
+				console.log('Switched to existing branch: ' + branchName);
+				this.HEAD = this.branches[i];
+				return this;
+			}
+		}
 
-    //checkout
-    Git.prototype.checkout = function (branchName) {
-       for (var i = this.branches.length ; i-- ;){
-            if(this.branches[i].name == branchName){
-                console.log("Swiched to existing branch"+branchName);
-                this.HEAD = this.branches[i];
-                return this;
-            }
-           else{
-               var newBranch = new Branch(branchName, this.HEAD.commit);
-               this.branches.push(newBranch);
-               this.HEAD = newBranch;
+		// If branch was not found, create a new one.
+		var newBranch = new Branch(branchName, this.HEAD.commit);
+		// Store branch.
+		this.branches.push(newBranch);
+		// Update HEAD
+		this.HEAD = newBranch;
 
-               console.log("Swiched to new branch: "+ branchName);
-               return this;
-           }
-       } 
-    }
+		console.log('Switched to new branch: ' + branchName);
+		return this;
+	}
 
+	/**
+	 * Logs history.
+	 * @return {Array} Commits in reverse chronological order.
+	 */
+	Git.prototype.log = function () {
+		// Start from HEAD commit
+		var commit = this.HEAD.commit,
+			history = [];
 
-    window.Git = Git;
+		while (commit) {
+			history.push(commit);
+			// Keep following the parent
+			commit = commit.parent;
+		}
 
+		return history;
+	};
 
-}
-
+	// Expose Git class on window.
+	window.Git = Git;
+})();
